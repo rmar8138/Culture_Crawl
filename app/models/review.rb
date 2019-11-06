f7aa9eee9d41eb981a581f0ad76f430bbc958b7b
rmar8138 <ragan.martinez@live.com>
2019-11-06 11:19:10 +1100

    Change rating data type to decimal, and add callback to review model to update crawl score

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :crawl
+  after_commit :update_rating
+
+  private
+
+  def update_rating
+    # update crawl rating after posting review
+    reviews_scores = self.crawl.reviews.pluck(:rating)
+    rating = nil
+    if reviews_scores.length > 0
+      # check if ratings exist first
+      rating = reviews_scores.reduce(:+) / reviews_scores.length
+    end
+    self.crawl.update(rating: rating)
+    pp "IT WORKED"
+    pp self.crawl
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_104922) do
+ActiveRecord::Schema.define(version: 2019_11_05_235927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,13 +50,13 @@ ActiveRecord::Schema.define(version: 2019_11_05_104922) do
     t.string "location"
     t.text "description"
     t.integer "price"
-    t.integer "rating"
     t.integer "max_attendees"
     t.boolean "finished"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "crawl_date"
+    t.decimal "rating"
     t.index ["user_id"], name: "index_crawls_on_user_id"
   end
 
