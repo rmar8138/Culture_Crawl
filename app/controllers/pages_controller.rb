eca020c194d612db653d44dadebfce4b5a68832e
rmar8138 <ragan.martinez@live.com>
2019-11-08 10:09:52 +1100

    Add basic styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @latest_crawls = Crawl.last(6)
+    @highest_rated_crawls = Crawl.where("rating > ?", 0).order(:rating).last(6)
   end
 end
